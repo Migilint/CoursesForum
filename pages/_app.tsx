2cd3fa79259f652acc302ba32fb8fff223e0315b
Vladimir <vovakay2003@gmail.com>
2023-04-29 00:43:32 +0300

    Added the component Button with ours classNames

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,7 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <link rel="icon" href="/favicon.ico"/>
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" />
-      <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&display=swap" rel="stylesheet" />
+      <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&display=swap" />
     </Head>
     <Component {...pageProps} />
     </>;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
-import {Htag} from "@/components";
+import {Button, Htag} from "@/components";
 
 export default function Home() {
 
   return (
       <div>
           <Htag tag='h1'>Text</Htag>
+          <Button appearance='primary'>Кнопка</Button>
+          <Button appearance='ghost'>Кнопка</Button>
       </div>
   );
 }
