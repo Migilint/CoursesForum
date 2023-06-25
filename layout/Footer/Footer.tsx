541523675d2065fd63d7d678caf73f443c9a75ed
Vladimir <vovakay2003@gmail.com>
2023-06-25 04:39:23 +0300

    I added a footer to this pet-project

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,12 +1,18 @@
-import {HeaderProps} from "@/layout/Header/Header.props";
 import styles from './Footer.module.css';
 import cn from "classnames";
+import {format} from 'date-fns';
+import {FooterProps} from "@/layout/Footer/Footer.props";
 
 
-export const Footer = ({...props }: HeaderProps): JSX.Element => {
+export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
+
     return (
-        <div {...props}>
-            Header
-        </div>
+        <footer className={cn(className, styles.footer)} {...props}>
+                <div>
+                    OwlTop © 2022 - {format(new Date(), 'yyyy')} Все права защищены
+                </div>
+                    <a href="#" target={"_blank"}>Пользовательское соглашение</a>
+                    <a href="#" target={"_blank"}>Политика конфиденциальности</a>
+        </footer>
     );
 };
