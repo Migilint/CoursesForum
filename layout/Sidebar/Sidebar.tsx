d64b09088fc0be6b71b6004cd4926993a95d7595
Vladimir <vovakay2003@gmail.com>
2023-06-28 17:22:04 +0300

    I added a Logo link

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -2,10 +2,14 @@ import {SidebarProps} from "@/layout/Sidebar/Sidebar.props";
 import styles from './Sidebar.module.css';
 import cn from "classnames";
 import {Menu} from "@/layout/Menu/Menu";
+import Logo from '../logo.svg';
+import Link from "next/link";
 
-export const Sidebar = ({...props }: SidebarProps): JSX.Element => {
+export const Sidebar = ({ className,  ...props }: SidebarProps): JSX.Element => {
     return (
-        <div {...props}>
+        <div className={cn(className, styles.sidebar)} {...props} >
+            <Link href={"/"}><Logo className={styles.logo}/></Link>
+            <div>Поиск</div>
             <Menu />
         </div>
     );
